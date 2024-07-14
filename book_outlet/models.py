bbc40b68828adad8e387a34c642b0afeeffe35c3
mo99me99 <m.m.hosseini1099@gmail.com>
2024-07-14 10:37:48 +0330

    create book model and migrate

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.PositiveSmallIntegerField()
+    
