f95da51ba531b95c20283810e11cadaf9001ae12
mo99me99 <m.m.hosseini1099@gmail.com>
2024-07-15 20:25:15 +0330

    Address model and one-to-one relation to Author

--- book_outlet/admin.py
+++ book_outlet/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Author, Address
 # Register your models here.
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['city', 'street', 'postal_code']
+    
+    
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ['title', 'author', 'rating', 'is_bestselling']
     list_filter = ['author', 'rating', 'is_bestselling']
     prepopulated_fields = {'slug': ('title',)}
-    
--- book_outlet/models.py
+++ book_outlet/models.py
@@ -7,13 +7,37 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
 
 
+class Address(models.Model):
+    street = models.CharField(max_length=50)
+    postal_code = models.CharField(max_length=10)
+    city = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return f'{self.city} {self.street}'
+    
+    class Meta :
+        verbose_name_plural = 'Address Entries'
+
+
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    address = models.OneToOneField(
+        Address, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self) -> str:
+        return f'{self.first_name} {self.last_name}'
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)
     slug = models.SlugField(unique=True, db_index=True)
     rating = models.PositiveSmallIntegerField(
         validators=[MinValueValidator(1), MaxValueValidator(5)]
     )
-    author = models.CharField(max_length=100, null=True)
+    author = models.ForeignKey(
+        Author, on_delete=models.CASCADE, null=True, related_name='books')
     is_bestselling = models.BooleanField(default=False)
 
     # def save(self, *args, **kwargs):
@@ -22,7 +46,6 @@ class Book(models.Model):
 
     def __str__(self) -> str:
         return f'{self.title} ({self.rating})'
-    
+
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.slug])
-    
